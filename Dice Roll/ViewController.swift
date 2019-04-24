24a68785afe130e817c0e048e95d7b5c78ea9867
JiaxinD <49501057+JiaxinD@users.noreply.github.com>
2019-04-24 15:41:47 -0700

    Initialization

---
+++ Dice Roll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by Jiaxin on 4/24/19.
+//  Copyright © 2019 Jiaxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
